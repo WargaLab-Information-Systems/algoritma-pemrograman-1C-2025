b8d85f42bb76aef52786f5ae65be2c06cc306e55
merinasagala03-star <merinasagala03@gmail.com>
2025-09-30 17:35:25 +0700

    Update MerrinaSagala-25_107.py

--- MerrinaSagala-25_107.py
+++ MerrinaSagala-25_107.py
@@ -1,36 +1,36 @@
-# SOAL NOMOR 1
-#Harga Barang
-# harga_buku = 5000
-# jumlah_buku = 3
-# harga_pensil = 4500
-# jumlah_pensil = 2
+#SOAL NOMOR 1
+Harga Barang
+harga_buku = 5000
+jumlah_buku = 3
+harga_pensil = 4500
+jumlah_pensil = 2
 
-# #Total harga barang sebelum pajak
-# total_harga = (harga_buku*jumlah_buku) + (harga_pensil*jumlah_pensil)
+#Total harga barang sebelum pajak
+total_harga = (harga_buku*jumlah_buku) + (harga_pensil*jumlah_pensil)
 
-# #Pajak 10%
-# pajak = total_harga * 0.10
+#Pajak 10%
+pajak = total_harga * 0.10
 
-# #Total bayar
-# total_bayar = total_harga + pajak
+#Total bayar
+total_bayar = total_harga + pajak
 
-# print ("total belanja sebelum pajak:", total_harga)
-# print ("pajak 10% :, pajak")
-# print ("total yang harus dibayar ;", total_bayar)
+print ("total belanja sebelum pajak:", total_harga)
+print ("pajak 10% :, pajak")
+print ("total yang harus dibayar ;", total_bayar)
 
 #SOAL NOMOR 2
-# panjang = int(input("masukkan panjang balok (cm):"))
-# lebar = int(input("Masukkan lebar balok(cm):"))
-# tinggi = int(input("Masukkan tinggi balok (cm):"))
+panjang = int(input("masukkan panjang balok (cm):"))
+lebar = int(input("Masukkan lebar balok(cm):"))
+tinggi = int(input("Masukkan tinggi balok (cm):"))
 
-# #Menghitung Volume 
-# volume = panjang*lebar*tinggi
-# #Menghitung Luas permukaan
-# luas_permukaan = 2*((panjang*lebar) + (panjang*tinggi) + (lebar*tinggi) )
+#Menghitung Volume 
+volume = panjang*lebar*tinggi
+#Menghitung Luas permukaan
+luas_permukaan = 2*((panjang*lebar) + (panjang*tinggi) + (lebar*tinggi) )
 
-# print ("Hasil perhitungan Balok:")
-# print ("Volume balok:", volume, "cm³")
-# print ("Luas permukaan balok:", luas_permukaan, "cm²")
+print ("Hasil perhitungan Balok:")
+print ("Volume balok:", volume, "cm³")
+print ("Luas permukaan balok:", luas_permukaan, "cm²")
 
 
 
@@ -48,4 +48,5 @@ kombinasi = numerator//denominator
 
 print (f"dumerator :{numerator}")
 print (f"denominator:{denominator}")
+
 print (f"kemungkinan kombinasi:{kombinasi}")
