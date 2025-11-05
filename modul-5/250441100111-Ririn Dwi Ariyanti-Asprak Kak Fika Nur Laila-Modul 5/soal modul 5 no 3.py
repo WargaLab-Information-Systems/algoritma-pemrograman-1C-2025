d95e4306f7a41a810290f21aabf5807e8ed82b3a
RirinDwi25 <ririndwi633@gmail.com>
2025-11-05 14:13:33 +0700

    Add files via upload

---
+++ modul-5/250441100111-Ririn Dwi Ariyanti-Asprak Kak Fika Nur Laila-Modul 5/soal modul 5 no 3.py
@@ -0,0 +1,27 @@
+def hitung_gaji(nama, jabatan, gaji_pokok):
+    pajak = 0.05 * gaji_pokok
+
+    if jabatan.lower() == "manager":
+        tunjangan = 0.10 * gaji_pokok
+    elif jabatan.lower() == "staff":
+        tunjangan = 0.05 * gaji_pokok
+    else :
+        tunjangan = 0
+        
+    gaji_bersih = gaji_pokok - pajak + tunjangan
+
+    print("\n== RINCIAN GAJI KARYAWAN ==")
+    print("==============================")
+    print(f"Nama Karyawan : {nama}")
+    print(f"Jabatan       : {jabatan}")
+    print(f"Gaji Pokok    : Rp {gaji_pokok:,.0f}")
+    print(f"Pajak (5%)    : Rp {pajak:,.0f}")
+    print(f"Tunjangan     : Rp {tunjangan:,.0f}")
+    print(f"Gaji Bersih   : Rp {gaji_bersih:,.0f}")
+    print("==============================")
+
+Nama = input("Masukkan nama karyawan: ")
+jabatan = input("Masukkan jabatan (Manager/Staff/lainnya): ")
+gaji_pokok = float(input("Masukkan gaji pokok: "))
+
+hitung_gaji(Nama, jabatan, gaji_pokok)
